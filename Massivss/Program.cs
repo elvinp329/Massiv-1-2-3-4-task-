b00def3a88f491b64c6d375aa509d814254caf48
Elvinp329 <elvinkj@code.edu.az>
2023-01-04 23:54:58 +0400

    MassivTask1234

---
+++ Massivss/Program.cs
@@ -0,0 +1,132 @@
+﻿using System.Runtime.Serialization.Formatters;
+
+namespace Massivss
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+
+        {
+            ////#region1
+            //Console.Write("Massivin uzunlugunu daxil edin");
+            //int n = int.Parse(Console.ReadLine());
+            //int[] arr = new int[n];
+            //int count = 0;
+            //int sum = 0;
+
+            //for (int i = 0; i < arr.Length; i++) 
+            //{
+            //    arr[i] = int.Parse(Console.ReadLine());
+
+
+            //}
+            // for ( int i= 0; i < arr.Length; i++) 
+
+            //    if (arr[i]< 0)
+            //    {
+            //        count++;
+            //        sum += arr[i];
+            //    }
+
+            //Console.WriteLine($"Massivin menfi elemetlerin cemi{sum}  sayi {count}");
+
+            ////#endregion1
+
+            //#region 2
+            //Console.Write("Massivin uzunlugunu daxil edin");
+            //int n = int.Parse(Console.ReadLine());
+            //int[] arr = new int[n];
+            //int count = 0;
+            //int sum = 0;
+
+
+            //for (int i = 0; i < arr.Length; i++)
+            //{
+            //    arr[i] = int.Parse(Console.ReadLine());
+
+
+            //}
+
+            //for (int i = 0; i < arr.Length; i++) 
+            //{
+            //    if (arr[i] % 5  == 0 && arr[i] % 7 != 0) 
+
+            //   {
+            //    count++;
+
+            //    sum += arr[i];
+
+            //    }
+
+
+
+            //}
+
+            //Console.WriteLine($"Massivin 5-bolunen ve 7-bolunmeyen elemetlerin cem {sum} ,  sayi {count}");
+            //endregion2
+
+            //#region3
+            //Console.Write("Massivin uzunlugunu daxil edin");
+            //int n = int.Parse(Console.ReadLine());
+            //int[] arr = new int[n];
+            //int count = 0;
+            //int sum = 0;
+
+            //for (int i = 0; i < arr.Length; i++)
+            //{
+            //    arr[i] = int.Parse(Console.ReadLine());
+
+
+            //}
+
+            //for (int i = 0; i < arr.Length; i++)
+            //{
+            //    if (arr[i] > 0 && arr[i] % 2 == 1)
+
+            //    {
+            //        count++;
+
+
+            //        sum += arr[i];
+
+            //    }
+
+
+            //}
+
+            //Console.WriteLine($"Massivin eyni zamanda musbet ve tek olan elemetlerin cemi {sum}  say {count}");
+            //#endregion3
+
+           // #region4
+           // Console.Write("Massivin uzunlugunu daxil edin");
+           // int n = int.Parse(Console.ReadLine());
+           // int[] arr = new int[n];
+           // int count = 0;
+           // int sum = 0;
+
+           // for(int i = 0; i < arr.Length; i++)
+           // {
+           //     arr[i] = int.Parse(Console.ReadLine());
+           // }
+
+           // for (int i = 0; i < arr.Length; i++)
+
+           // {
+
+           //     if (i % 2 == 1 && arr[i] % 2 == 0)
+
+
+           //     {
+           //         count++;
+           //         sum += arr[i];
+           //     }
+
+           // }
+
+           // Console.WriteLine($"Massivin cut yerde olan cut elemetnlerin cemi{sum} say {count}");
+           //#endregion4
+
+
+        }
+    }
+}
